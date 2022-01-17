09b57d0ff6d9e2a5b7b691488d65457cef2ccb04
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-01-16 21:00:01 -0300

    quesao-teste

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 
+
+import styles from '../styles/Home.module.css'
 import Questao from '../components/questao'
+import questaoModel from '../model/questao'
+import RespostaModel from '../model/resposta'
 
 export default function Home() {
+  const Questaoteste = new questaoModel(1, "melhor cor?", [
+      RespostaModel.errada("verde"),
+      RespostaModel.errada("preto"),
+      RespostaModel.errada("branco"),
+      RespostaModel.certa("roxo"),
+  ])
+
+
   return (
-    <Questao />
+    <Questao valor={Questaoteste}/>
   )
 }
