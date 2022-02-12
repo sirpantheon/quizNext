6e50e7f4702040068e21b330bd1005b5cfd01b47
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-02-12 20:25:11 -0300

    carregando questoes

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,14 +11,34 @@ const questaoteste = new QuestaoModel(1, "melhor cor?", [
     RespostaModel.certa("roxo"),
 ])
 
+const BASE_URL ="HTTP://localhost:3000/api"
+
 export default function Home() {
   
-  const [questao,setQuestao] = useState(questaoteste)
+
+  const [idsQuestoes,setIdsQuestoes] = useState<number[]>([])
+  const [questao,setQuestao] = useState<QuestaoModel>(questaoteste)
   const questaoRef = useRef<QuestaoModel>()
 
   useEffect(() => {
     questaoRef.current = questao
+    carregadorQuestoesIds()
   }, [questao])
+
+  async function carregadorQuestoesIds() {
+    const resp = await fetch( `${BASE_URL}/questionario`)
+    const idsQuestoes = await resp.json()
+    setIdsQuestoes(idsQuestoes)
+  }
+
+  async function carregadorQuestoes(idQuestoes:number) {
+    const resp = await fetch( `${BASE_URL}/questoes/${idQuestoes}`)
+    const idsQuestoes = await resp.json()
+    setIdsQuestoes(idsQuestoes)
+  }
+
+  
+
 
   function onResponse(questao:QuestaoModel){
 
@@ -27,15 +47,6 @@ export default function Home() {
   function irPraProximoPasso(){}
 
   return (
-
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh'
-    }}>
-
     <Questionario 
       questao={questao}
       ultima={true}
@@ -43,8 +54,6 @@ export default function Home() {
       irPraProximoPasso={irPraProximoPasso}
     />
 
-      
-
-    </div>
+    
   )
 }
